0d4aebcfe0ecf93b9cf442929682cb6acafa2d5f
lucastolon <liltolon@gmail.com>
2024-05-04 17:04:55 +0200

    Camera web, shot button to base64 url on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import Webcam from "react-webcam";
 function App() {
+
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"/>
+
+        <div className="container">
+            <button className="centered-button" onClick={capture}>Shot</button>
+             {/*{imgSrc && (
+                <img
+                    src={imgSrc}
+                />
+            )}*/}
+            <p> {imgSrc} </p>
+        </div>
+    </div>);
+
 }
+
+// Crear boton que cuando se haga click, hacer la foto
+// Convertir la foto a bas64 y obtener el tipo de fichero
+// Enviar la imagen al backend
+//Hacer una consulta rest a un servidor (backend)
+
 
 export default App;
